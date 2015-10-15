3ff173d069b33ad452deae38726ca0921a33f403
Metallion <akieckens@axsh.net>
2015-10-15 16:24:12 +0900

    Added lxc logging for debugging purposes

--- dcmgr/run.sh
+++ dcmgr/run.sh
@@ -4,5 +4,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "${SCRIPT_DIR}/vmspec.conf"
 
 CONSOLE_LOG=${SCRIPT_DIR}/console.log
+LXC_LOG=${SCRIPT_DIR}/lxc.log
 
-sudo lxc-start -n "${CONTAINER_NAME}" -L "${CONSOLE_LOG}"
+sudo lxc-start -n "${CONTAINER_NAME}" -L "${CONSOLE_LOG}" --logfile "${LXC_LOG}"
