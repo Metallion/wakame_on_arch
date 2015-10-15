07573956bed1f96f7b7a32b6333814685e41077b
Metallion <akieckens@axsh.net>
2015-10-15 14:38:16 +0900

    Added a script to run the container and a .gitignore file for the console log

---
+++ dcmgr/run.sh
@@ -0,0 +1,8 @@
+set -eux
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+CONTAINER_NAME=dcmgr
+CONSOLE_LOG=${SCRIPT_DIR}/console.log
+
+sudo lxc-start -n "${CONTAINER_NAME}" -L "${CONSOLE_LOG}"
