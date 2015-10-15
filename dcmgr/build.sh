97f99a016a487f888af5611ecee3bfd37871e46f
Metallion <akieckens@axsh.net>
2015-10-15 15:21:59 +0900

    Moved the rootfs rsync command down to after wakame installation

--- dcmgr/build.sh
+++ dcmgr/build.sh
@@ -11,8 +11,6 @@ sudo lxc-create -t centos -n "${CONTAINER_NAME}"
 WAKAME_REPO_URL='https://raw.githubusercontent.com/axsh/wakame-vdc/master/rpmbuild/yum_repositories/wakame-vdc-develop.repo'
 sudo curl -o "${ROOTFS}"/etc/yum.repos.d/wakame-vdc-develop.repo -R "${WAKAME_REPO_URL}"
 
-sudo rsync -av "${SCRIPT_DIR}"/rootfs/ "${ROOTFS}/"
-
 export PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/centos/bin
 sudo chroot "${ROOTFS}" /bin/bash -ex <<'EOS'
   usermod -U root
@@ -24,5 +22,7 @@ sudo chroot "${ROOTFS}" /bin/bash -ex <<'EOS'
   yum install -y epel-release
   yum install -y wakame-vdc-dcmgr-vmapp-config
 EOS
+
+sudo rsync -av "${SCRIPT_DIR}"/rootfs/ "${ROOTFS}/"
 
 sudo cp "${SCRIPT_DIR}/lxc_config" "${LXC_DIR}/${CONTAINER_NAME}/config"
